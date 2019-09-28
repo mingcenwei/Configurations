baf4448997906de43e7102c4ddf63265342022d4
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-09-28 13:42:57 +0800

    Add the firewall configurations

--- SSH/configure.fish
+++ SSH/configure.fish
@@ -2,22 +2,27 @@
 
 # Set error codes
 set --local BACK_UP_FILES_IS_NOT_LOADED_ERROR_CODE 1
+set --local UFW_IS_NOT_INSTALLED_ERROR_CODE 2
+set --local NOT_ROOT_ERROR_CODE 3
 
-set DIR (dirname (realpath (status --current-filename)))
+set --local source_dir (dirname (realpath (status --current-filename)))'/Files'
+set --local local_ssh_dir "$HOME"'/.ssh'
+set --local global_sshd_dir '/etc/ssh'
 
 # Make sure that "back_up_files" is loaded
-functions back_up_files > /dev/null 2>&1
+functions back_up_files > '/dev/null' 2>&1
 or begin
     echo 'Error: "back_up_files" function is not loaded!' >&2
     exit "$BACK_UP_FILES_IS_NOT_LOADED_ERROR_CODE"
 end
 
-# For clients
+### For clients
 function _configure_ssh_client
 
 end
+###
 
-# For servers
+### For servers
 function _configure_ssh_server
     if test -e '/etc/ssh/sshd_config'
     or test -L '/etc/ssh/sshd_config'
@@ -27,12 +32,52 @@ function _configure_ssh_server
     mkdir -p /etc/ssh >/dev/null 2>&1
     cp
 end
+###
 
-echo -- 'Client or server?'
-read --prompt-str='client/server: ' --local client_or_server
+### Configure firewall, using "ufw"
+function _configure_firewall
+    # Make sure that "ufw" is installed
+    command -v ufw > '/dev/null' 2>&1
+    or begin
+        echo 'Error: "ufw" is not installed! Please install the program' >&2
+        exit "$UFW_IS_NOT_INSTALLED_ERROR_CODE"
+    end
+
+    # Make sure that we're the root user
+    test (whoami) = 'root'
+    or begin
+        echo 'Error: You are not the root user! Please run "su"' >&2
+        exit "$NOT_ROOT_ERROR_CODE"
+    end
+
+    ufw reset
+    ufw default deny incoming
+    ufw default allow outgoing
+    ufw default deny routed
+
+    # Limit/allow tcp ports for incoming traffic from "ssh" and "shadowsocks"
+    read --prompt-str='Please enter the ssh port' --local ports
+    for port in $ports
+        ufw limit in "$port"'/tcp'
+    end
+    read --prompt-str='Please enter the shadowsocks ports' --local ports
+    for port in $ports
+        ufw allow in "$port"'/tcp'
+    end
+
+    ufw logging medium
+    ufw enable
+    ufw status verbose
+end
+###
+
+### Ask whether it's a client or a server
+echo -- 'Client or server or both?'
+read --prompt-str='client/server/both: ' --local client_or_server
 set client_or_server (string lower "$client_or_server")
-while not contains "$client_or_server" 'client' 'server'
-    read --prompt-str='Please enter either "client" or "server": ' client_or_server
+while not contains "$client_or_server" 'client' 'server' 'both'
+# and test -n "$client_or_server"
+    read --prompt-str='Please enter "client" or "server" or "both": ' client_or_server
     set client_or_server (string lower "$client_or_server")
 end
 
@@ -41,17 +86,10 @@ switch "$client_or_server"
         _configure_ssh_client
     case 'server'
         _configure_ssh_server
+    case 'both'
+        _configure_ssh_server
+        _configure_ssh_client
 end
+###
 
-functions --erase _configure_ssh_client _configure_ssh_server
-
-# # Back up former vim configurations
-# for file in '.vim' '.vimrc' '.viminfo'
-#     if test -e {$HOME}'/'{$file}
-#     or test -L {$HOME}'/'{$file}
-#         back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source {$HOME}'/'{$file}
-#     end
-# end
-
-# mkdir -p {$HOME}'/.vim'
-# ln -si {$DIR}'/Files/vimrc' {$HOME}'/.vim/vimrc'
+functions --erase _configure_ssh_client _configure_ssh_server _configure_firewall
