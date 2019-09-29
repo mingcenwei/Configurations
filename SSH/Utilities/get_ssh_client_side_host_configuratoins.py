2d62fb47ca8c9424fd443a9fbbde715082994986
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-09-29 14:41:19 +0800

    Add client-side ssh configurations

---
+++ SSH/Utilities/get_ssh_client_side_host_configuratoins.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Used to get client-side host configurations from ~/.ssh/config"""
+
+from sys import argv
+from pathlib import Path
+
+DELIMITER = argv[1]
+
+with open(Path.home() / ".ssh/config") as config_file:
+    in_wildcard_block: bool = False
+    for line in config_file:
+        if line[0: 4] == "Host":
+            host = line[5: ].strip()
+            if host == "*":
+                in_wildcard_block = True
+                continue
+            else:
+                in_wildcard_block = False
+                print(DELIMITER)
+                print(line, end="")
+                continue
+        elif in_wildcard_block:
+            continue
+        elif line.strip() == "":
+            continue
+        else:
+            print(line, end="")
--- SSH/configure.fish
+++ SSH/configure.fish
@@ -4,14 +4,15 @@
 umask 077
 
 # Set error codes
-set --local BACK_UP_FILES_IS_NOT_LOADED_ERROR_CODE 1
-set --local SSH_IS_NOT_INSTALLED_ERROR_CODE 2
-set --local UFW_IS_NOT_INSTALLED_ERROR_CODE 3
-set --local NOT_ROOT_ERROR_CODE 4
+set BACK_UP_FILES_IS_NOT_LOADED_ERROR_CODE 1
+set SSH_IS_NOT_INSTALLED_ERROR_CODE 2
+set UFW_IS_NOT_INSTALLED_ERROR_CODE 3
+set NOT_ROOT_ERROR_CODE 4
 
-set --local source_dir (dirname (realpath (status --current-filename)))'/Files'
-set --local local_ssh_dir "$HOME"'/.ssh'
-set --local global_sshd_dir '/etc/ssh'
+set source_dir (dirname (realpath (status --current-filename)))'/Files'
+set utility_dir (dirname (realpath (status --current-filename)))'/Utilities'
+set local_ssh_dir "$HOME"'/.ssh'
+set global_sshd_dir '/etc/ssh'
 
 # Make sure that "back_up_files" is loaded
 functions back_up_files > '/dev/null' 2>&1
@@ -29,20 +30,100 @@ end
 
 ### For clients, add ~/.ssh/config
 function _configure_ssh_client
+    # Back up former ssh client user private configurations if available
+    set --local get_ssh_client_side_host_configuratoins "$utility_dir"'/get_ssh_client_side_host_configuratoins.py'
+    chmod u+x "$get_ssh_client_side_host_configuratoins"
+    set --local delimiter '---'
+    set --local lines
+    if test -e "$HOME"'/.ssh/config'
+        "$get_ssh_client_side_host_configuratoins" "$delimiter" | while read --local line
+            set lines $lines "$line"
+        end
+    end
 
+    ### Track the configuration file with "track_file" function
+    set --local track_file_dir_path "$track_file_DIR_PATH"
+    test -z "$track_file_dir_path"
+    and set track_file_dir_path "$HOME"'/.my_private_configurations'
+    set --local template_file "$track_file_dir_path"'/SSH_config'
+
+    # Back up former template file
+    if test -e "$template_file"
+    or test -L "$template_file"
+        back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$template_file"
+    end
+
+    touch "$template_file"
+    # For security
+    chmod 600 "$template_file"
+
+    # Use previous ssh client user private configurations if available
+    for line in $lines[2..-1]
+        if test "$line" = "$delimiter"
+            echo >> "$template_file"
+        else
+            echo "$line" >> "$template_file"
+        end
+    end
+    echo >> "$template_file"
+
+    # Ask the user to add new client-side host configurations
+    echo 'Adding new hosts'
+    read --prompt-str='Host (enter empty string to end): ' --local host
+    set host (string trim "$host")
+    while test -n "$host"
+        read --prompt-str='Hostname: ' --local hostname
+        set hostname (string trim "$hostname")
+        read --prompt-str='User: ' --local user
+        set user (string trim "$user")
+        read --prompt-str='Port (enter empty string to use the default): ' --local port
+        set port (string trim "$port")
+        read --prompt-str='IdentityFile (enter empty string to use the default): ' --local identity_file
+        set identity_file (string trim "$identity_file")
+
+        if test -z "$hostname"
+            echoerr '"Hostname" cannot be empty'
+        else if test -z "$user"
+            echoerr '"User" cannot be empty'
+        else
+            echo 'Host '"$host" >> "$template_file"
+            echo '    Hostname '"$hostname" >> "$template_file"
+            echo '    User '"$user" >> "$template_file"
+            test -n "$port"
+            and echo '    Port '"$port" >> "$template_file"
+            test -n "$identity_file"
+            and echo '    IdentityFile '"$identity_file" >> "$template_file"
+            echo
+        end
+
+        read --prompt-str='Host (enter empty string to end): ' host
+        set host (string trim "$host")
+    end
+
+    # Back up former ssh client configurations
+    set --local config_file "$local_ssh_dir"'/config'
+    if test -e "$config_file"
+    or test -L "$config_file"
+        back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$config_file"
+    end
+
+    cat "$source_dir"'/config' >> "$template_file"
+    and ln -si "$template_file" "$local_ssh_dir"'/config'
+    and track_file --template=(basename "$template_file") --actual="$local_ssh_dir"'/config' --check
+    ###
 end
 ###
 
 ### For servers, add /etc/ssh/sshd_config
 function _configure_ssh_server
-    # Back up former server sshd configurations
-    if test -e "$global_sshd_dir"'/sshd_config'
-    or test -L "$global_sshd_dir"'/sshd_config'
-        back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$global_sshd_dir"'/sshd_config'
-    end
+    # # Back up former server sshd configurations
+    # if test -e "$global_sshd_dir"'/sshd_config'
+    # or test -L "$global_sshd_dir"'/sshd_config'
+    #     back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$global_sshd_dir"'/sshd_config'
+    # end
 
-    mkdir -p /etc/ssh >/dev/null 2>&1
-    cp
+    # mkdir -p /etc/ssh >/dev/null 2>&1
+    # cp
 end
 ###
 
