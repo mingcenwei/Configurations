33d4b5eb4545a02ba063c217d4fe1fc5a6861bff
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-10-01 04:31:01 +0800

    Bug fix

--- Fish/Files/non_fisher_fish_configurations/set_default_working_directory.fish
+++ Fish/Files/non_fisher_fish_configurations/set_default_working_directory.fish
@@ -4,14 +4,16 @@
 if status is-login
 and status is-interactive
     ### Set default working directory
-    if is_platform 'android-termux'
-    and test ! -d "$HOME"'/Downloads'
-
+    if test ! -d "$HOME"'/Downloads'
         test -e "$HOME"'/Downloads'
         or test -L "$HOME"'/Downloads'
         and back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$HOME"'/Downloads'
 
-        ln -si '/sdcard/Download' "$HOME"'/Downloads'
+        if is_platform 'android-termux'
+            ln -si '/sdcard/Download' "$HOME"'/Downloads'
+        else
+            mkdir -p "$HOME"'/Downloads'
+        end
     end
     cd ~/Downloads
     ###
