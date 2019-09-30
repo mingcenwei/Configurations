f95a08df2342d5428bafbb29e66f2e03227eb7a1
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-09-30 12:56:59 +0800

    Change permissions of the files to 600 or 700

--- configure.fish
+++ configure.fish
@@ -1,7 +1,15 @@
 #!/usr/bin/env fish
 
 # For security
-chmod -- 700 **
+for file in **
+    if test -d "$file"
+    or test (string sub -s '-5' "$file") = '.fish'
+    or test (string sub -s '-3' "$file") = '.py'
+        chmod -- 700 "$file"
+    else
+        chmod -- 600 "$file"
+    end
+end
 
 set --local current_dir (dirname (realpath (status --current-filename)))
 set --local my_backups "$current_dir"'/.my_backups'
