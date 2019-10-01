703563e41bcce34056f76a7ba42c0932329d090b
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-10-01 20:59:17 +0800

    Bug fix

--- Fish/Files/fish_configurations/conf.d/set_android_termux.fish
+++ Fish/Files/fish_configurations/conf.d/set_android_termux.fish
@@ -1,13 +1,33 @@
 #!/usr/bin/env fish
 
-### Set "~/sdcard" directory
 if is_platform 'android-termux'
-and test ! -d "$HOME"'/sdcard'
+    # Set "~/sdcard" directory
+    if test ! -d "$HOME"'/sdcard'
+        test -e "$HOME"'/sdcard'
+        or test -L "$HOME"'/sdcard'
+        and back_up_files --back-up --timestamp --destination --compressor \
+            --suffix --parents --remove-source "$HOME"'/sdcard'
 
-    test -e "$HOME"'/sdcard'
-    or test -L "$HOME"'/sdcard'
-    and back_up_files --back-up --timestamp --destination --compressor --suffix --parents --remove-source "$HOME"'/sdcard'
+        ln -si '/sdcard' "$HOME"'/sdcard'
+    end
 
-    ln -si '/sdcard' "$HOME"'/sdcard'
+    # Set "~/Downloads" directory
+    if test ! -d "$HOME"'/Downloads'
+        test -e "$HOME"'/Downloads'
+        or test -L "$HOME"'/Downloads'
+        and back_up_files --back-up --timestamp --destination --compressor \
+            --suffix --parents --remove-source "$HOME"'/Downloads'
+
+        mkdir -p "$HOME"'/Downloads'
+    end
+
+    # Set "~/Downloads/Download" directory
+    if test ! -d "$HOME"'/Downloads/Download'
+        test -e "$HOME"'/Downloads/Download'
+        or test -L "$HOME"'/Downloads/Download'
+        and back_up_files --back-up --timestamp --destination --compressor \
+            --suffix --parents --remove-source "$HOME"'/Downloads/Download'
+
+        ln -si '/sdcard/Download' "$HOME"'/Downloads/Download'
+    end
 end
-###
