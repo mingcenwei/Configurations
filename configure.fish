3d63a247fa6bdf2a299346db8c7528b324acbc82
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-10-02 14:01:49 +0800

    Resolve the dependency issue

--- configure.fish
+++ configure.fish
@@ -16,13 +16,33 @@ end
 
 set --local my_backups "$PACKAGE_DIR"'/.my_backups'
 set --local my_private_configurations "$PACKAGE_DIR"'/.my_private_configurations'
+set --local config_dir_list \
+    'Fish' \
+    'Vim' \
+    'Git' \
+    'SSH' \
+    'GPG' \
+    'macOS'
 
 test -L "$my_backups"
 or ln -si "$HOME"'/.my_backups' "$my_backups"
 test -L "$my_private_configurations"
 or ln -si "$HOME"'/.my_private_configurations' "$my_private_configurations"
 
-for script in "$PACKAGE_DIR"/*'/configure.fish'
+for script in "$PACKAGE_DIR"'/'*'/configure.fish'
+    not contains (basename (dirname "$script")) $config_dir_list
+    and echoerr \
+        'Please add "'(basename (dirname "$script"))'" to $config_dir_list'
+end
+
+
+for config_dir in config_dir_list
+    set --local script "$PACKAGE_DIR"'/'"$config_dir"'/configure.fish'
+
+    not test -e "$script"
+    and echoerr "$script"' isn\'t found'
+    and continue
+
     echo 'Run '"$script"'?'
     read_until --variable='yes_or_no' --default-position=1 'yes' 'no'
     if test "$yes_or_no" = 'yes'
