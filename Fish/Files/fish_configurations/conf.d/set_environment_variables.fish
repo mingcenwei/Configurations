852901f819f47119d705b70b7b715362fe1e6f54
sAy <47605337+mingcenwei@users.noreply.github.com>
2019-12-02 03:03:18 +0800

    Update for haskell-stack on macOS

--- Fish/Files/fish_configurations/conf.d/set_environment_variables.fish
+++ Fish/Files/fish_configurations/conf.d/set_environment_variables.fish
@@ -2,6 +2,6 @@
 
 ### Set macOS path variables
 if is_platform 'macos'
-    set --path PATH $PATH '/Users/say/Library/Python/3.7/bin'
+    set --path PATH $PATH "$HOME"'/Library/Python/3.7/bin' "$HOME"'/.local/bin'
 end
 ###
