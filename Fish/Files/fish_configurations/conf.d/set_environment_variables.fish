8a0b16753014ae594077bb6a52067a083e180192
sAy <47605337+mingcenwei@users.noreply.github.com>
2020-02-19 17:25:25 +0800

    Set npm prefix

--- Fish/Files/fish_configurations/conf.d/set_environment_variables.fish
+++ Fish/Files/fish_configurations/conf.d/set_environment_variables.fish
@@ -13,11 +13,13 @@ if is_platform 'macos'
         'Python3.7-bin' \
         'Haskell-stack-bin'
 
+    # Make sure previous 2 settings are correct
     if not test (count $paths) = (count $filenames)
         echoerr 'Lengths of $paths and $filenames are not equal'
         exit "$LENGTHS_NOT_EQUAL_ERROR_CODE"
     end
 
+    # Backup and create '/etc/paths.d/'"$filenames[$iii]"
     for iii in (seq (count $paths))
         set --local filepath '/etc/paths.d/'"$filenames[$iii]"
 
@@ -37,5 +39,11 @@ if is_platform 'macos'
             or echo "$paths[$iii]" | sudo tee "$filepath" > '/dev/null'
         end
     end
+end
+###
+
+### Set npm prefix in $PATH
+if is_platform 'npm'
+    set PATH $PATH "$HOME"'/.my_global_node_modules'
 end
 ###
