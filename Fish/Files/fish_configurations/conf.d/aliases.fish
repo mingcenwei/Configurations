04b3fb0270b613aececd1ba103223d1010c2a411
sAy <47605337+mingcenwei@users.noreply.github.com>
2020-09-19 19:30:16 +0800

    Add fish aliases

---
+++ Fish/Files/fish_configurations/conf.d/aliases.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+# Interactive mode (prompting for confirmation)
+alias rmi='rm -i'
+alias cpi='cp -i'
+alias mvi='mv -i'
+alias lni='ln -i'
+
+# Use "exa" instead of "ls"/"ll"/"la" or "tree"
+if command -v exa > '/dev/null'
+	alias ls='exa'
+	alias ll='exa --long --binary --group --links --time-style=long-iso --git'
+	alias la='exa --all --long --binary --group --links --time-style=long-iso --git'
+	alias laa='exa --all --all --long --binary --group --links --time-style=long-iso --git'
+	alias tree='exa --tree'
+else
+	echoerr --warning 'Please install "exa"'
+end
