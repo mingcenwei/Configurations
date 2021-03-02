b5e30941e3cefdb3f78653c38a78287febfe58c8
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-03-02 11:15:56 +0800

    Update `remove-legacy.fish`

--- TODO/remove-legacy.fish
+++ TODO/remove-legacy.fish
@@ -10,8 +10,12 @@ for var in $fisherUniversalVariables
 	set --erase --universal "$var"
 end
 
-back-up-files --remove-source -- \
-	"$HOME"/.ssh/.my_ssh_agent_information \
-	"$HOME"/.my_backups \
-	"$HOME"/.my_private_configurations \
-	"$HOME"/.say-local/.ssh-agent-info
+set --local filesToRemove
+for file in "$HOME"/.ssh/.my_ssh_agent_information \
+"$HOME"/.my_backups \
+"$HOME"/.my_private_configurations \
+"$HOME"/.say-local/.ssh-agent-info
+	test -e "$file"
+	and set filesToRemove "$file"
+end
+back-up-files --remove-source -- $filesToRemove
