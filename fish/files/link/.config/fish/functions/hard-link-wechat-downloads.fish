2b0ff950eec155d320d3a80941e5f16f0766c02c
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-03-18 20:20:11 +0800

    Fix bugs

--- fish/files/link/.config/fish/functions/hard-link-wechat-downloads.fish
+++ fish/files/link/.config/fish/functions/hard-link-wechat-downloads.fish
@@ -2,21 +2,21 @@
 
 function hard-link-wechat-downloads \
 --description 'Hard link WeChat downloaded files on Android'
-	is-platform android-termux || exit 2
-	
+	is-platform android-termux || return 2
+
 	if not set --local rootId (sudo id -u)
 	or test 0 -ne "$rootId"
 		echo-err 'You are not a sudoer'
-		exit 2
+		return 2
 	end
 
 	set --local prefix '/data/media/0'
 	set --local from "$prefix"'/Android/data/com.tencent.MicroMsg/Download'
 	set --local to "$prefix"'/Download/.WeChat.say-local'
 
-	if not test -d "$from"
+	if not sudo test -d "$from"
 		echo-err "Not a directory: "(string escape -- "$from")
-		exit 2
+		return 2
 	end
 
 	sudo rsync --archive --hard-links --sparse \
