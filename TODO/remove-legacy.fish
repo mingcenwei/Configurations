0f331c3624eccd80c1fa712ac4468a3de052cfb3
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-04-03 01:21:14 +0800

    Remove legacy fish variable

--- TODO/remove-legacy.fish
+++ TODO/remove-legacy.fish
@@ -3,6 +3,7 @@
 set --erase --universal track_file_DIR_PATH
 set --erase --universal _Configurations__PATH
 set --erase --universal JAVA_HOME
+set --erase --universal fish_color_match
 
 set --local fisherUniversalVariables \
 	(set --names --universal | grep '_fisher_')
