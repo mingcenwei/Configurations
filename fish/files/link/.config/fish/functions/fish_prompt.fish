32c2c619fde14f7a05ed4f2f9062bd5f5d1cc4c6
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2021-04-03 01:43:49 +0800

    Update `fish_prompt.fish`

--- fish/files/link/.config/fish/functions/fish_prompt.fish
+++ fish/files/link/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,14 @@
 #!/usr/bin/env fish
 
 # See "Abbreviations for long hostnames" in the code
+# See `env HOME=(mktemp -d) XDG_CONFIG_HOME= fish --command 'type fish_prompt'`
 
 function fish_prompt --description 'Write out the prompt'
 	set --local lastPipeStatus $pipestatus
-	set --local normal (set_color normal)
+	set --local normal (set_color 'normal')
+
+	# Export for __fish_print_pipestatus.
+	set --local --export __fish_last_status $status
 
 	### Abbreviations for long hostnames
 	set --local theHostname (prompt_hostname)
@@ -15,9 +19,8 @@ function fish_prompt --description 'Write out the prompt'
 
 	# Color the prompt differently when we're root
 	set --local colorCwd $fish_color_cwd
-	set --local prefix
 	set --local suffix '>'
-	if contains -- $USER root toor
+	if fish_is_root_user
 		if set --query fish_color_cwd_root
 			set colorCwd $fish_color_cwd_root
 		end
@@ -31,7 +34,16 @@ function fish_prompt --description 'Write out the prompt'
 	end
 
 	# Write pipestatus
-	set --local promptStatus (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $lastPipeStatus)
+	# If the status was carried over (e.g. after `set`), don't bold it.
+	set --local boldFlag '--bold'
+	set --query __fish_prompt_status_generation
+	or set --global __fish_prompt_status_generation $status_generation
+	if test $__fish_prompt_status_generation = $status_generation
+        set boldFlag
+    end
+	set __fish_prompt_status_generation $status_generation
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $colorHost) "$theHostname" $normal ' ' (set_color $colorCwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $promptStatus $suffix " "
+	set --local promptStatus (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $boldFlag $fish_color_status) $lastPipeStatus)
+
+	echo -n -s (set_color $fish_color_user) "$USER" $normal '@' (set_color $colorHost) "$theHostname" $normal ' ' (set_color $colorCwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal ' '$promptStatus $suffix ' '
 end
