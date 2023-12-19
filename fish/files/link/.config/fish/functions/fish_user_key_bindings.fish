7814545092338fd15055e53ef3922639b11a4680
Mingcen Wei (sAy) <47605337+mingcenwei@users.noreply.github.com>
2023-12-18 18:17:28 -0600

    Fix `fzf_key_bindings` bug

--- fish/files/link/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/files/link/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@
 
 function fish_user_key_bindings
 	# For "fzf"
-	if check-dependencies --program 'fzf'
+	if check-dependencies --program 'fzf' && check-dependencies --function 'fzf_key_bindings'
 		fzf_key_bindings
 	end
 end
